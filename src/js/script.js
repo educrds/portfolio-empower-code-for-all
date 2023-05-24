d355afdd1f1ea0511025efcdeeb34893d94cc4b1
eduardo <eduardocardoso1039@gmail.com>
2023-05-24 17:35:00 -0300

    feat(Added Menu Mobile)

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,14 @@
+// Funcionalidade Toggle Mode
 const themeButton = document.getElementById('theme-button');
-    const body = document.querySelector('body');
+const body = document.querySelector('body');
 
-    themeButton.addEventListener('click', () => {
-      console.log("ola");
-      body.classList.toggle('theme-dark');
-      body.classList.toggle('theme-light');
-    });
+themeButton.addEventListener('click', () => {
+  body.classList.toggle('theme-dark');
+  body.classList.toggle('theme-light');
+});
+
+// Funcionalidade Menu Mobile
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navElements = document.querySelector('.nav-elements');
+
+navbarToggle.addEventListener('click', () => navElements.classList.toggle('active'));
